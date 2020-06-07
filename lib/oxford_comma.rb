d9e2b6d9ead1074757fba9655454334b52200344
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-07 15:16:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,7 +9,7 @@ def oxford_comma(array)
   else
     counter = array.length - 2
     result = array[0..counter].join(", ")
-    result = result + "and "
+    result = result + " and " + array[counter + 1]
   end 
   return result
 end
