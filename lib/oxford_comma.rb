1436c7ea596535815056be5c67a3098d7a6cfb56
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-07 15:13:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,12 @@
 def oxford_comma(array)
   result = ""
-  when array.length === 1
+  counter = array.length
+  case counter
+  when 1
     result << array[0]
-  when array.length === 2
+  when 2
     result = array[0] + " and " + array[1]
-  when array.length > 2
+  when > 2
     counter = array.length - 2
     result = array[0..counter].join(", ")
     result = result + "and " + array.[array.length - 1]
