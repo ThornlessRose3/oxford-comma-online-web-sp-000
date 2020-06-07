52dcd2dbe824e17959abfa1d34aad3abfcf5fb9c
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-07 15:11:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   result = ""
-  when array.length == 1
+  when array.length === 1
     result << array[0]
-  when array.length == 2
+  when array.length === 2
     result = array[0] + " and " + array[1]
   when array.length > 2
     counter = array.length - 2
